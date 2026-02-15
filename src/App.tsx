7029563d5881c7cf623499b0385cedcc1da82310
michael <mgoldie@udel.edu>
2026-02-15 12:18:37 -0500

    Added Hello World to App.tsx.

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App(): React.JSX.Element {
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload. Michael.
+                automatically reload. Michael. Hello World.
             </p>
         </div>
     );
